199482734661ae786e6184fc71ddaa527e7604b4
Extazy <iqramhuq2006@gmail.com>
2025-12-28 15:31:27 +0600

    Delete Inheritance/Inheritance (2)/Inheritance(3) directory

--- Inheritance/Inheritance (2)/Inheritance(3)/RunStudent.java
+++
@@ -1,6 +0,0 @@
-public class RunStudent {
-    public static void main(String[] args) {
-        Student s = new Student("Iqram", 20, 101);
-        System.out.println(s.getName() + " " + s.getAge() + " " + s.getId());
-    }
-}
